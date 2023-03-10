0cd85579d16acaac7f29872f574c03eebeaba311
Jeric <62868605+JericNarte@users.noreply.github.com>
2023-03-10 23:51:37 +0800

    v0.1.0
    landing-top

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Header() {
     return (
         <nav className='sticky top-0 bg-slate-900 h-16 flex justify-between items-center px-10'>
-            <div className='font-A font-bold color-B text-4xl'><a href="#overview">ABCHECKERS</a></div>
-            <ul className='font-A font-bold color-B text-xl flex gap-10'>
+            <div className='font-A font-bold color-B text-4xl'><a href="#overview">CHECKERS🏁</a></div>
+            <ul className='font-A font-bold color-B text-xl hidden lg:flex gap-10'>
                 <li><a href="#overview">OVERVIEW</a></li>
                 <li><a href="#overview">DATA</a></li>
                 <li><a href="#overview">METHODS</a></li>
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
+import Logo from '../images/logo.png';
+
 export default function Landing() {
     return (
-        <div className='h-screen bg-slate-600'>Landing</div>
+        <section className='min-h-screen flex flex-col items-center bg-slate-100 py-14 lg:py-52 '>
+            <div className='flex flex-col gap-5 max-w-3xl px-10 lg:max-w-6xl lg:flex-row '>
+                <img className='w-60 h-60 self-center' src={Logo} alt="Logo" />
+                <span>
+                    <div className='lg:flex font-A font-bold color-A text-center lg:text-left my-5'>
+                        <div className='flex items-end justify-center text-6xl tracking-tighter'>Hi! We are&nbsp;</div>
+                        <div>
+                            <span className='text-violet-900 text-7xl'>A</span>
+                            <span className='text-violet-900 text-7xl'>B</span>
+                            <span className='text-violet-900 text-7xl'>C</span>
+                            <span className='text-6xl'>heckers</span>
+                            <span className='text-6xl'>🏁</span>
+                        </div>
+                    </div>
+                    <div className='font-B text-justify text-xl pr-0 lg:pr-12'>
+                        This is our project, Misinformation Regarding the Country's Inflation and Weakening Peso. Our aim is to use data analysis techniques to look into Twitter data relevant to this topic.
+                    </div>
+                </span>
+            </div>
+        </section>
     );
 }
