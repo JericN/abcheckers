ddf6b2a3f3d903baa8b50ce44898a99c99b63541
Jeric <62868605+JericNarte@users.noreply.github.com>
2023-03-12 16:02:35 +0800

    0.2.1
    overview 1/2 done

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,23 +1,19 @@
 import React from 'react';
 
 export default function Overview() {
+    const css_label = 'font-A font-bold text-3xl w-44 text-center lg:text-right';
+    const css_text = 'font-B text-md text-justify';
     return (
-        <div className='min-h-screen py-14 bg-black text-white lg:py-52'>
-            <div>OVERVIEW</div>
-            <div>
-                <div>
-                    <h3>MOTIVATION</h3>
-                    <p>The tweets that contain mis/disinformation have no effect regarding the people's views on the government when it comes to our weakening peso and rising inflation. The tweets that contain mis/disinformation have no effect regarding the people's views on the government when it comes to our weakening peso and rising inflation. The tweets that contain mis/disinformation have no effect regarding the people's views on the government when it comes to our weakening peso and rising inflation.</p>
-                </div>
-                <div>
-                    <h3>SOLUTION</h3>
-                    <p>The tweets that contain mis/disinformation have no effect regarding the people's views on the government when it comes to our weakening peso and rising inflation. The tweets that contain mis/disinformation have no effect regarding the people's views on the government when it comes to our weakening peso and rising inflation. The tweets that contain mis/disinformation have no effect regarding the people's views on the government when it comes to our weakening peso and rising inflation.</p>
-                </div>
-                <div>
-                    <h3>PROBLEM</h3>
-                    <p>The tweets that contain mis/disinformation have no effect regarding the people's views on the government when it comes to our weakening peso and rising inflation. The tweets that contain mis/disinformation have no effect regarding the people's views on the government when it comes to our weakening peso and rising inflation. The tweets that contain mis/disinformation have no effect regarding the people's views on the government when it comes to our weakening peso and rising inflation.</p>
-                </div>
+        <section className='flex flex-col items-center min-h-screen text-white bg-black py-28 lg:py-52'>
+            <h2 className='mb-16 font-bold font-A text-7xl'>OVERVIEW</h2>
+            <div className='grid pr-0 mx-10 justify-items-center lg:w-5/6 lg:pr-10 xl:w-2/3 lg:grid-cols-2t gap-y-10'>
+                <h3 className={css_label}>MOTIVATION</h3>
+                <p className={css_text}>The tweets that contain mis/disinformation have no effect regarding the people's views on the government when it comes to our weakening peso and rising inflation. The tweets that contain mis/disinformation have no effect regarding the people's views on the government when it comes to our weakening peso and rising inflation. The tweets that contain mis/disinformation have no effect regarding the people's views on the government when it comes to our weakening peso and rising inflation.</p>
+                <h3 className={css_label}>SOLUTION</h3>
+                <p className={css_text}>The tweets that contain mis/disinformation have no effect regarding the people's views on the government when it comes to our weakening peso and rising inflation. The tweets that contain mis/disinformation have no effect regarding the people's views on the government when it comes to our weakening peso and rising inflation. The tweets that contain mis/disinformation have no effect regarding the people's views on the government when it comes to our weakening peso and rising inflation.</p>
+                <h3 className={css_label}>PROBLEM</h3>
+                <p className={css_text}>The tweets that contain mis/disinformation have no effect regarding the people's views on the government when it comes to our weakening peso and rising inflation. The tweets that contain mis/disinformation have no effect regarding the people's views on the government when it comes to our weakening peso and rising inflation. The tweets that contain mis/disinformation have no effect regarding the people's views on the government when it comes to our weakening peso and rising inflation.</p>
             </div>
-        </div>
+        </section>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        '2t': '200px auto'
+      }
+    },
   },
   plugins: [],
 };
